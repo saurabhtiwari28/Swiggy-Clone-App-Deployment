e32deee80fa7f8c428ed52c889ed09034e369d83
saurabh <saurabhtiwari2811@gmail.com>
2024-12-26 01:37:14 +0530

    Add Terraform code

---
+++ Terraform/main.tf
@@ -0,0 +1,70 @@
+resource "aws_instance" "web"{
+  ami = "ami-0e2c8caa4b6378d8c"
+  instance_type = "t2.large"
+  key_name = "github-actions"
+  user_data = base64encode(file("userdata.sh"))
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "Swiggy Server"
+  }
+
+  root_block_device {
+    volume_size = 30
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "Project-SG"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  
+  tags = {
+    Name = "Project-SG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "jenkins" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8080
+  ip_protocol       = "tcp"
+  to_port           = 8080
+}
+
+resource "aws_vpc_security_group_ingress_rule" "sonarqube" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 9000
+  ip_protocol       = "tcp"
+  to_port           = 9000
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
